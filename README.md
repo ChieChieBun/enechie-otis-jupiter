69b9bb59ee7c4e91335b4940eee6793fa794e953
ChieChie <104299226+ChieChieBun@users.noreply.github.com>
2025-02-07 20:40:29 -0500

    form and functionality added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # enechie-otis-jupiter
 Intro to Programming course with Code the Dream
 E'Ne'Chie Otis
+
+[My Open API Project](https://github.com/ChieChieBun/enechie-open-api)
--- js/index.js
+++ js/index.js
@@ -7,9 +7,9 @@ const thisYear = today.getFullYear();
 const copyright = document.createElement(`p`);
 copyright.innerHTML = `<em>E'Ne'Chie Otis</em> ${thisYear} &#169;` ;
 footer.appendChild(copyright);
-var skills = ["Painting", "HTML", "CSS", "Javascript", "Github"];
+let skills = ["Painting", "HTML", "CSS", "Javascript", "Github"];
 const skillsSection = document.querySelector(`#Skills`);
-var skillsList = skillsSection.querySelector(`ul`);
+let skillsList = skillsSection.querySelector(`ul`);
 
 for (let i = 0; i < skills.length; i++) {
   const skill = document.createElement(`li`);
@@ -17,28 +17,27 @@ for (let i = 0; i < skills.length; i++) {
   skillsList.appendChild(skill);
 }
 
-var messageForm = document.querySelector(`[name="leave_message"]`);
+let messageForm = document.querySelector(`[name="leave_message"]`);
 messageForm.addEventListener(`submit`, event =>{
   event.preventDefault();
-  var usersName= event.target.usersName.value;
-  var usersEmail= event.target.usersEmail.value;
-  var usersMessage= event.target.usersMessage.value;
-  var messageSection = document.querySelector(`#Messages`);
-  var messageList = messageSection.querySelector(`ul`);
-  var newMessage = document.createElement(`li`);
+  let usersName= event.target.usersName.value;
+  let usersEmail= event.target.usersEmail.value;
+  let usersMessage= event.target.usersMessage.value;
+  let messageSection = document.querySelector(`#Messages`);
+  let messageList = messageSection.querySelector(`ul`);
+  let newMessage = document.createElement(`li`);
   newMessage.innerHTML= `<a href="mailto:${usersEmail}">${usersName}</a><span>${usersMessage}</span>`;
 
-  var removeButton = document.createElement(`button`);
+  let removeButton = document.createElement(`button`);
   removeButton.innerHTML= `remove`;
   removeButton.setAttribute("type", "button");
 
   removeButton.addEventListener("click", function () {
-    var entry = removeButton.parentNode;
+    let entry = removeButton.parentNode;
     entry.remove();
   });
   newMessage.appendChild(removeButton);
   messageList.appendChild(newMessage);
   console.log(messageList);
   messageForm.reset();
-
 } );
